8b1dafbef6da0ad3ff30857752cf9dfb7154533d
Reza Yahya <yayapahlevi@gmail.com>
2023-11-09 00:25:54 +0700

    splash screen with lottiefiles

---
+++ app/src/main/java/com/example/contohss/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.contohss
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
